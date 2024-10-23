a38a9c816843a56c285b33b2a2ab9613dc1e3112
VitaliiFedDev <sandineyer3@gmail.com>
2024-10-23 16:39:00 +0300

    feat: update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
